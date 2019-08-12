50c8838302b73a9942befffa65b2c3a5cf709f01
zxwei <2971652817@qq.com>
2019-08-12 14:43:15 +0800

    删除不必要的文件

--- DroppableComponent.js
+++
@@ -1,9 +0,0 @@
-import { useDrop } from 'react-dnd'
-
-function DroppableComponent(props) {
-  const [collectedProps, drop] = useDrop({
-    accept,
-  })
-
-  return <div ref={drop}>Drop Target</div>
-}
